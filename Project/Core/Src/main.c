10b61b1f52ba41e16ec6b67e55b8815f161bb51f
HTH1406\haithien <hathienhai4@gmail.com>
2024-09-10 11:51:17 +0700

    Update for Ex8 continue

--- Project/Core/Src/main.c
+++ Project/Core/Src/main.c
@@ -72,51 +72,39 @@ void clearAllClock(){
 void setNumberOnClock(int num) {
 	switch (num) {
 	case 0:
-		clearAllClock();
 		HAL_GPIO_WritePin(ZERO_GPIO_Port, ZERO_Pin, 0);
 		break;
 	case 1:
-		clearAllClock();
 		HAL_GPIO_WritePin(ONE_GPIO_Port, ONE_Pin, 0);
 		break;
 	case 2:
-		clearAllClock();
 		HAL_GPIO_WritePin(TWO_GPIO_Port, TWO_Pin, 0);
 		break;
 	case 3:
-		clearAllClock();
 		HAL_GPIO_WritePin(THREE_GPIO_Port, THREE_Pin, 0);
 		break;
 	case 4:
-		clearAllClock();
 		HAL_GPIO_WritePin(FOUR_GPIO_Port, FOUR_Pin, 0);
 		break;
 	case 5:
-		clearAllClock();
 		HAL_GPIO_WritePin(FIVE_GPIO_Port, FIVE_Pin, 0);
 		break;
 	case 6:
-		clearAllClock();
 		HAL_GPIO_WritePin(SIX_GPIO_Port, SIX_Pin, 0);
 		break;
 	case 7:
-		clearAllClock();
 		HAL_GPIO_WritePin(SEVEN_GPIO_Port, SEVEN_Pin, 0);
 		break;
 	case 8:
-		clearAllClock();
 		HAL_GPIO_WritePin(EIGHT_GPIO_Port, EIGHT_Pin, 0);
 		break;
 	case 9:
-		clearAllClock();
 		HAL_GPIO_WritePin(NINE_GPIO_Port, NINE_Pin, 0);
 		break;
 	case 10:
-		clearAllClock();
 		HAL_GPIO_WritePin(TEN_GPIO_Port, TEN_Pin, 0);
 		break;
 	case 11:
-		clearAllClock();
 		HAL_GPIO_WritePin(ELEVEN_GPIO_Port, ELEVEN_Pin, 0);
 		break;
 	}
@@ -160,6 +148,7 @@ int main(void)
   int cnt = 0;
   while (1)
   {
+	  clearAllClock();
 	  if (cnt >= 12) cnt = 0;
 	  setNumberOnClock(cnt++);
 	  HAL_Delay(1000);
